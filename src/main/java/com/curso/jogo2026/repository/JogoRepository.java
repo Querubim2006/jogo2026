d9b656661e55b4d0393beda35f0e49d89feaee86
Vitor Querubim <vitorquerubim2006@gmail.com>
2026-09-17 19:46:57 -0300

    fix: ultimas correções

--- src/main/java/com/curso/jogo2026/repository/JogoRepository.java
+++ src/main/java/com/curso/jogo2026/repository/JogoRepository.java
@@ -2,6 +2,7 @@ package com.curso.jogo2026.repository;
 
 import com.curso.jogo2026.domain.Jogo;
 import com.curso.jogo2026.domain.Status;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,9 +10,14 @@ import java.util.Optional;
 
 public interface JogoRepository extends JpaRepository<Jogo, Long> {
 
-    Optional<Jogo> findByCodigoBarras(String codigoBarras);
-
     boolean existsByCodigoBarras(String codigoBarras);
+
+    @EntityGraph(attributePaths = {"genero", "desenvolvedora"})
+    Optional<Jogo> findById(Long id);
+
+    @Override
+    @EntityGraph(attributePaths = {"genero", "desenvolvedora"})
+    List<Jogo> findAll();
 
     List<Jogo> findByGeneroId(Long generoId);
 
